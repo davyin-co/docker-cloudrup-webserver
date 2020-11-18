54a5dfa3dd7ef7b7d6889dd7fa90d052294f8ff1
terry zhang <zterry@qq.com>
2020-11-18 13:22:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# 介绍
+站群系统使用的webserver的docker镜像，基于ubuntu 18.04,安装配置了apache/php/ssh等。
---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+### https://blog.dockbit.com/templating-your-dockerfile-like-a-boss-2a84a67d28e9
+render() {
+    sedStr="
+    s/%%PHP_VERSION%%/$version/g;
+    "
+    sed -e "$sedStr" $1
+}
+
+versions=(7.1 7.2 7.3 7.4)
+for version in ${versions[*]}; do
+  if [ ! -d $version ]; then
+    mkdir $version
+  fi
+  render Dockerfile.template > $version/Dockerfile
+done
