8fb82db2bb6dbdea7b1b6c598324fbae36fd318a
terry zhang <zterry@qq.com>
2020-12-05 01:19:48 +0000

    oci8 suuport

--- update.sh
+++ update.sh
@@ -7,12 +7,17 @@ render() {
     sed -e "$sedStr" $1
 }
 
-versions=(7.1 7.2 7.3 7.4 8.0)
+versions=(7.1 7.2 7.3 7.4)
 for version in ${versions[*]}; do
-  if [ ! -d $version ]; then
-    mkdir $version
+  if [ ! -d ${version} ]; then
+    mkdir ${version}
   fi
-  render Dockerfile.template > $version/Dockerfile
+  render Dockerfile.template > ${version}/Dockerfile
+  ##OCI8
+  if [ ! -d ${version}/oci8 ]; then
+    mkdir ${version}/oci8
+  fi
+  render Dockerfile-oci8.template > $version/oci8/Dockerfile
   ## https://php.watch/versions/8.0/xmlrpc
   if [ "$version" = "8.0" ]; then
     sed -i "s/php\$PHP_VERSION-json/php-json/g" $version/Dockerfile
